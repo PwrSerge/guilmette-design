aed1808b6255a8db22b040fae0f47ffac18d2329
Serge Guilmette <sergeguilmette@gmail.com>
2014-08-27 16:20:13 -0400

    fixed menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,70 +1,79 @@
 /*global describe:true*/
 
 /*
-  Slidemenu
+  Header  -- Slide menu
 */
 
 $(function() {
     var $page = $('.inner-wrapper'),
         $navToggle = $('.nav-toggle'),
         $mainNavigation = $('.main-navigation'),
-        $header = $('.main-navigation'),
+        $header = $('.header-container'),
         $headerHt = $header.height(),
         $menuHt = $mainNavigation.height(),
         $totalmenuHt = $headerHt + $menuHt,
         $main = $('main');
 
+    //fixed  header   on scroll
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 6) {
 
-    if ($('.nav-toggle').css('display') === 'none') {
-        console.log('test');
-        $mainNavigation.css({
-            '-webkit-transform': 'translateY(0)',
-            '-ms-transform': 'translateY(0)',
-            'transform': 'translateY(0)'
-        });
-
-    } else {
-        $mainNavigation.css({
-            '-webkit-transform': 'translateY(-' + $menuHt + 'px)',
-            '-ms-transform': 'translateY(-' + $menuHt + 'px)',
-            'transform': 'translateY(-' + $menuHt + 'px)'
-        });
-
-    }
-
-
-
-    $navToggle.on('click', function() {
-        $page.toggleClass('open');
-
-        if ($page.hasClass('open')) {
-            $mainNavigation.css({
-                '-webkit-transform': 'translateY(0)',
-                '-ms-transform': 'translateY(0)',
-                'transform': 'translateY(0)'
-            });
-
-
-            $('.inner-wrapper.open').css({
-                '-webkit-transform': 'translateY(' + $menuHt + 'px)',
-                '-ms-transform': 'translateY(' + $menuHt + 'px)',
-                'transform': 'translateY(' + $menuHt + 'px)'
-            });
+            $header.addClass("header-container-fixed");
         } else {
-            $('.inner-wrapper').removeAttr('style');
-            $mainNavigation.css({
-                '-webkit-transform': 'translateY(-' + $menuHt + 'px)',
-                '-ms-transform': 'translateY(-' + $menuHt + 'px)',
-                'transform': 'translateY(-' + $menuHt + 'px)'
-            });
-
-            $('.inner-wrapper').css({
-                '-webkit-transform': 'translateY(-' + $menuHt + 'px)',
-                '-ms-transform': 'translateY(-' + $menuHt + 'px)',
-                'transform': 'translateY(-' + $menuHt + 'px)'
-            });
+            $header.removeClass("header-container-fixed");
         }
     });
+
+
+    // Toggle Slide Menu
+    // if ($('.nav-toggle').css('display') === 'none') {
+    //     console.log('test');
+    //     $mainNavigation.css({
+    //         '-webkit-transform': 'translateY(0)',
+    //         '-ms-transform': 'translateY(0)',
+    //         'transform': 'translateY(0)'
+    //     });
+
+    // } else {
+    //     $mainNavigation.css({
+    //         '-webkit-transform': 'translateY(-' + $menuHt + 'px)',
+    //         '-ms-transform': 'translateY(-' + $menuHt + 'px)',
+    //         'transform': 'translateY(-' + $menuHt + 'px)'
+    //     });
+
+    // }
+
+
+    // $navToggle.on('click', function() {
+    //     $page.toggleClass('open');
+
+    //     if ($page.hasClass('open')) {
+    //         $mainNavigation.css({
+    //             '-webkit-transform': 'translateY(0)',
+    //             '-ms-transform': 'translateY(0)',
+    //             'transform': 'translateY(0)'
+    //         });
+
+    //         $('.inner-wrapper.open').css({
+    //             '-webkit-transform': 'translateY(' + $menuHt + 'px)',
+    //             '-ms-transform': 'translateY(' + $menuHt + 'px)',
+    //             'transform': 'translateY(' + $menuHt + 'px)'
+    //         });
+    //     } else {
+    //         $('.inner-wrapper').removeAttr('style');
+    //         $mainNavigation.css({
+    //             '-webkit-transform': 'translateY(-' + $menuHt + 'px)',
+    //             '-ms-transform': 'translateY(-' + $menuHt + 'px)',
+    //             'transform': 'translateY(-' + $menuHt + 'px)'
+    //         });
+
+    //         $('.inner-wrapper').css({
+    //             '-webkit-transform': 'translateY(-' + $menuHt + 'px)',
+    //             '-ms-transform': 'translateY(-' + $menuHt + 'px)',
+    //             'transform': 'translateY(-' + $menuHt + 'px)'
+    //         });
+    //     }
+    // });
 
     // $main.on('click', function(e) {
     //     $('.inner-wrapper').removeAttr('style');
